5799a0bf931702cc89bc878b9ba35c59819d20dd
hippieperro <hippiechaka@MacBook-Pro-de-Armando.local>
2017-06-07 03:43:34 -0500

    scrollmagic

--- sections_es/end.php
+++ sections_es/end.php
@@ -2,6 +2,12 @@
 <script>window.jQuery || document.write('<script src="js/jquery-1.12.4.min.js"><\/script>')</script>
 <script src="js/owl.carousel.min.js"></script>
 <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+<!-- scrollmagic -->
+<script type="text/javascript" src="js/lib/greensock/TweenMax.min.js"></script>
+<script src="js/scrollmagic/minified/ScrollMagic.min.js"></script>
+<script type="text/javascript" src="js/scrollmagic/uncompressed/plugins/animation.gsap.js"></script>
+
 <script src="js/main.js"></script>
     
 <!-- GoogleAnalytics -->
