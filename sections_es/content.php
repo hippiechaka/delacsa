3500f8a4ded3bd36edc89d83ad40614e1a128f8e
Mafiass <churro.mafiass@gmail.com>
2017-06-08 21:15:16 -0500

    textos

--- sections_es/content.php
+++ sections_es/content.php
@@ -5,7 +5,7 @@
 		<div class="container">
 			<div class="navbar-header">
 				<a href="#top" class="navbar-brand">
-					<img src="http://placehold.it/110x60" alt="">
+					<img src="images/delacsa_logo2.png" alt="">
 				</a>
 			</div>
 			<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#nav-res" aria-expanded="true">
@@ -78,69 +78,69 @@
 		<div class="halfContainer pull-left">
 			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
 			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<p>Nuestros expertos en fontanería y plomería mantendran los espacios limpios, secos y seguros para su máximo aprovechamiento.</p>
+			<p>Mantenimiento, prevención y corrección. Tuberías, baños, cocinas, llaves de paso, drenaje, malos olores, fugas de agua, goteras, bombas de agua. Limpieza de tinacos y cisternas. Tanques y tuberías de gas, boilers.</p>	
 		</div>
 	</div>
 	<div id="serv3" class="col-md-6 left">
 		<img src="images/electricidad2.png" alt="" class="imgServicio img-left">
 		<div class="halfContainer pull-right">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<i class="servIcon"><img src="images/electricidad.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Electricidad</h3>	
+			<p>La seguridad es uno de los elementos mas preciados en nuestra vida diaria. Los servicios de electricidad ofrecen mejorar la seguridad para toda persona en el área asignada.</p>
+			<p>Mantenimiento, prevención y corrección. Aparatos eléctricos, instalaciónes eléctricas, restauración, intercomunicadores, portones eléctricos, ahorro energético, paneles solares y energía verde.</p>	
 		</div>
 	</div>
 	<div id="serv4" class="col-md-6 right">
 		<img src="images/albanileria2.png" alt="" class="imgServicio img-right">
 		<div class="halfContainer pull-left">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<i class="servIcon"><img src="images/albanileria.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Abañilería</h3>	
+			<p>Los maestros de obra con su amplia experiencia, les darán el mejor servicio, con un presupuesto óptimo y excelentes tiempos de entrega.</p>
+			<p>Mantenimiento, prevención y corrección. Obra negra y gris, demoliciones, enjarres, bóbedas, tablaroca, pisos, muros, techos, azulejos.</p>	
 		</div>
 	</div>
 	<div id="serv5" class="col-md-6 left">
 		<img src="images/herreria2.png" alt="" class="imgServicio img-left">
 		<div class="halfContainer pull-right">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<i class="servIcon"><img src="images/herreria.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Herrería</h3>	
+			<p>El manejo del metal en frío o caliente, de manera industrial o artística ayudará a embellecer y optimizar cualquier trabajo requerido.</p>
+			<p>Mantenimiento, prevención y corrección. Hierro forjado, rejas, estructuras, soldadura, terrazas, muebles, escaleras. Andamios, perfiles, herramientas.</p>	
 		</div>
 	</div>
 	<div id="serv6" class="col-md-6 right">
 		<img src="images/impermeabilizacion2.png" alt="" class="imgServicio img-right">
 		<div class="halfContainer pull-left">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<i class="servIcon"><img src="images/impermeabilizacion.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Impermeabilización</h3>	
+			<p>Somos especialistas en impermebilizar azoteas.</p>
+			<p>Utilizamos materiales duraderos para garantizar nuestro trabajo.</p>	
 		</div>
 	</div>
 	<div id="serv7" class="col-md-6 left">
 		<img src="images/carpinteria2.png" alt="" class="imgServicio img-left">
 		<div class="halfContainer pull-right">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<i class="servIcon"><img src="images/carpinteria.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Carpintería</h3>	
+			<p>El arte de trabajar la madera no se le puede dejar a cualquier persona. Nosotros ofrecemos trabajos de excelente calidad en todo momento.</p>
+			<p>Mantenimiento, prevención y corrección. Cocinas, closets, ventanas, pisos, mobiliario puertas, pérgolas, decks, terrazas.</p>	
 		</div>
 	</div>
 	<div id="serv8" class="col-md-6 right">
 		<img src="images/azulejos2.png" alt="" class="imgServicio img-right">
 		<div class="halfContainer pull-left">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
-			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
-			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
+			<i class="servIcon"><img src="images/azulejos.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Instalación de Pisos y Azulejos</h3>	
+			<p>Instalamos recubrimientos tales como azulejos, mosaicos y marmol.</p>
+			<p>Garantizamos nuestras instalaciones contra filtraciones, huecos entre el piso y suelo.</p>	
 		</div>
 	</div>
 	<div id="serv9" class="col-md-6 left">
 		<img src="images/remodelaciones2.png" alt="" class="imgServicio img-left">
 		<div class="halfContainer pull-right">
-			<i class="servIcon"><img src="images/fontaneria.png" alt="" class="pull-left imgServicio"></i>
-			<h3 align="center">fontanería</h3>	
+			<i class="servIcon"><img src="images/remodelaciones.png" alt="" class="pull-left imgServicio"></i>
+			<h3 align="center">Remodelaciones</h3>	
 			<p>Transformamos el ambiente de espacios por medio de colores para mejorar la calidad del espacio y la interacción con los usuarios.</p>
 			<p>Mantenimiento, prevención y corrección fachadas, techos, pisos, mobiliario. Impermeabilización en azoteas. Corrección en humedad y salitre</p>	
 		</div>
